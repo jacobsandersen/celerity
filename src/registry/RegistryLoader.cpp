fd1f3d78cce09d7a6f96db184d75aa98413d4d2d
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2025-08-15 23:12:02 +0800

    Update gitignore

---
+++ src/registry/RegistryLoader.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Jacob Andersen on 8/15/25.
+//
+
+#include "RegistryLoader.h"
+
+namespace celerity {
+namespace registry {
+} // registry
+} // celerity
---
+++ src/registry/RegistryLoader.h
@@ -0,0 +1,18 @@
+//
+// Created by Jacob Andersen on 8/15/25.
+//
+
+#ifndef CELERITY_REGISTRYLOADER_H
+#define CELERITY_REGISTRYLOADER_H
+
+namespace celerity {
+namespace registry {
+
+class RegistryLoader {
+
+};
+
+} // registry
+} // celerity
+
+#endif //CELERITY_REGISTRYLOADER_H
